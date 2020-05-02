071eed211a3348d398735500531de01221428ea7
Bobby Stanley <bsta09@gmail.com>
2020-05-02 15:09:59 -0400

    Added comments

--- ring.py
+++ ring.py
@@ -12,6 +12,11 @@ class Ring:
         self.light = 'blue'
 
     def contact_check(self):
+        """ Check all contacts in dict, and cross-check for contacts 
+            within past 14 days
+
+            Change ring2's light to flashing red for >1 contacs, warning
+            warning yellow for 1 contact """
         for key in self.contacts.keys():
             most_recent_contact = self.contacts[key]['contact_date'][-1]
             delta = date.today() - most_recent_contact
@@ -29,11 +34,13 @@ class Ring:
         self.contacts[ring2]['status'] = ring2.status
 
     def increment_contact(self, ring2):
+        """ Log contact with another ring and increment amount as dict value """
         self.contacts[ring2]['contact_count'] += 1
         self.contacts[ring2]['contact_date'].append(self.date)
         self.contacts[ring2]['status'] = ring2.status
         
     def contact(self, ring2, date):
+        """ To be used when 2 rings come within arbitrary contact"""
         self.date = date
         if ring2 in self.contacts.keys():
             self.increment_contact(ring2)
@@ -41,6 +48,8 @@ class Ring:
             self.first_contact(ring2)
 
     def status_change(self):
+        """ To be used when wearer receives positive result sends warnings to close
+            contacts via change in receiver's light"""
         if self.status == False:
             self.status = True
             self.light = 'flashing red'
